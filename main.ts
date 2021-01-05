08a8405d4bf0153de8c80d08d0f878fad7c1b37d
Eduardo Maciel <mrgamex49@gmail.com>
2021-01-04 22:43:56 -0300

    I am learning how to define variables with types, and with this learning English

---
+++ main.ts
@@ -0,0 +1,8 @@
+export {}
+let message :string = 'Hello Word'
+console.log(message);
+
+let isBeginner : boolean = false;
+let name : string = "Eduardo";
+//name = true => this code result in error because variable name is typed as string and not boolean
+let sentence : string = `Hello My name is ${name}, i am a beginner in Typescript but i am better in English `;
