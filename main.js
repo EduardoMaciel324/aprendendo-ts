97d0a6a8389ade1112a6b3095562274e16e31c4f
Eduardo Maciel <mrgamex49@gmail.com>
2021-01-05 21:41:44 -0300

    I learned how to create classes in typescript and tested if i can write override methods, but i learned that is not possible

--- main.js
+++ main.js
@@ -2,3 +2,28 @@
 exports.__esModule = true;
 var message = 'Hello Word';
 console.log(message);
+var isBeginner = false;
+var name = "Eduardo";
+//name = true => this code result in error because variable name is typed as string and not boolean
+var sentence = "Hello My name is " + name + ", i am a beginner in Typescript but i am better in English ";
+var multiType;
+multiType = 10;
+multiType = "Eduardo";
+multiType = true;
+var Person = /** @class */ (function () {
+    function Person(id, name) {
+        this.id = id;
+        this.name = name;
+        this.date = new Date();
+    }
+    Person.prototype.displayInfo = function () {
+        console.log(this.id + " " + this.name + " " + this.date);
+    };
+    return Person;
+}());
+function hasValue(man) {
+    man = new Person(1, 'Eduardo');
+    man.displayInfo();
+}
+var man;
+hasValue(man);
--- main.ts
+++ main.ts
@@ -6,3 +6,32 @@ let isBeginner : boolean = false;
 let name : string = "Eduardo";
 //name = true => this code result in error because variable name is typed as string and not boolean
 let sentence : string = `Hello My name is ${name}, i am a beginner in Typescript but i am better in English `;
+
+let multiType : number | boolean | string;
+multiType = 10;
+multiType = "Eduardo";
+multiType = true;
+
+class Person {
+    id : number;
+    name : string;
+    date : Date;
+    constructor(id : number, name : string) {
+        this.id = id;
+        this.name = name;
+        this.date = new Date();
+    }
+    displayInfo() : void {
+        console.log(`${this.id} ${this.name} ${this.date}`);
+    }
+ }
+
+function hasValue (man : Person) : void {
+    man = new Person(1,'Eduardo');
+    man.displayInfo();
+}
+var man : Person;
+hasValue(man);
+
+
+
